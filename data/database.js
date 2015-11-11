2442d55c7c7b94f019e2765cc953e6d6787f668a
Ryan Blakeley <rbblakeley@gmail.com>
2015-11-11 12:01:55 -0700

    updated mock database

--- data/database.js
+++ data/database.js
@@ -1,4 +1,5 @@
 /**
+ *  Copyright (c) 2015, Ryan Blakeley
  *  Copyright (c) 2015, Facebook, Inc.
  *  All rights reserved.
  *
@@ -9,25 +10,49 @@
 
 // Model types
 class User extends Object {}
-class Widget extends Object {}
+class Role extends Object {}
+class Space extends Object {}
+class Tool extends Object {}
+// class Project extends Object {}
 
 // Mock data
-var viewer = new User();
-viewer.id = '1';
-viewer.name = 'Anonymous';
-var widgets = ['What\'s-it', 'Who\'s-it', 'How\'s-it'].map((name, i) => {
-  var widget = new Widget();
-  widget.name = name;
-  widget.id = `${i}`;
-  return widget;
+var users = ['Anonymous', 'Bob', 'Jane'].map((name, i) => {
+  var user = new User();
+  user.name = name;
+  user.id = `${i}`;
+  return user;
+});
+var roles = ['Leader', 'Engineer', 'Hands'].map((name, i) => {
+  var role = new Role();
+  role.name = name;
+  role.id = `${i}`;
+  return role;
+});
+var spaces = ['Plot', 'Greenhouse', 'Building'].map((name, i) => {
+  var space = new Space();
+  space.name = name;
+  space.id = `${i}`;
+  return space;
+});
+var tools = ['Rake', 'Shovel', 'Barrel'].map((name, i) => {
+  var tool = new Tool();
+  tool.name = name;
+  tool.id = `${i}`;
+  return tool;
 });
 
 module.exports = {
   // Export methods that your schema can use to interact with your database
-  getUser: (id) => id === viewer.id ? viewer : null,
-  getViewer: () => viewer,
-  getWidget: (id) => widgets.find(w => w.id === id),
-  getWidgets: () => widgets,
+  getUser: (id) => users.find(u => u.id === id),
+  getViewer: () => users[0],
+  getRole: (id) => roles.find(r => r.id === id),
+  getRoles: () => roles,
+  getSpace: (id) => spaces.find(s => s.id === id),
+  getSpaces: () => spaces,
+  getTool: (id) => tools.find(t => t.id === id),
+  getTools: () => tools,
   User,
-  Widget,
+  Role,
+  Space,
+  Tool
 };
